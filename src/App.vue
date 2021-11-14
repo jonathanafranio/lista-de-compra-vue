c906cf69f670ef605d89045522f5b7be958b77d1
jonathan particular <jonathanp@jonathan-550XDA.local>
2021-11-14 00:16:37 -0300

    removendo alguns comentarios

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,18 @@
     <BgImagem></BgImagem>
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <ListaProdutos></ListaProdutos>
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-
   </div>
 </template>
 
 <script>
 import BgImagem from './components/BgImagem.vue'
 import ListaProdutos from './components/ListaProdutos.vue'
-//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     BgImagem,
     ListaProdutos,
-    //HelloWorld,
   }
 }
 </script>
