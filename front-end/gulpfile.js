3edc067daee9fbd8f6ad3a4aebd696c246a5a05e
jonathan particular <jonathanp@jonathan-550XDA.local>
2021-11-14 04:25:04 -0300

    arquivo do gulp no diretorio front-end para o sass

---
+++ front-end/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
+
+function compile(){
+	return(
+    gulp.src('./assets/sass/*.sass')
+        .pipe(sassGlob())
+        .pipe(sass({outputStyle:'compressed'}))
+        //.pipe(sass({outputStyle: 'expanded', sourceComments: true}))
+		    .pipe(gulp.dest('./assets/css/'))
+	)
+}
+exports.compile = compile;
+
+function watchfiles(){
+	gulp.watch('./assets/sass/**', compile);
+}
+const watch = gulp.parallel(watchfiles)
+exports.watch = watch;
