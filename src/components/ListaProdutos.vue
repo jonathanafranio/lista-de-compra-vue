e9a03ccf4e59750a08bca8ca8c2f4a41c42f5c50
Jonathan Afranio <jonathan.afranio.s@gmail.com>
2021-11-26 22:40:35 -0300

    modal de alerta de duplicidade de produto na lista

--- src/components/ListaProdutos.vue
+++ src/components/ListaProdutos.vue
@@ -1,81 +1,90 @@
 <template>
-	<div class="container">
-        <IncludeItem v-on:add-prod="addProduct"></IncludeItem>
-        <hr>
-        <ul class="list mx-12">
-            <li v-if="products.length <= 0">
-                <h3>Sua lista está vazia.</h3>
-            </li>
-            <li class="list__prod list__title" v-else>
-                <strong class="list__name-prod mx-3">
-                    Produto:
-                </strong>
+    <div>
+        <div class="container">
+            <IncludeItem v-on:add-prod="hasProd"></IncludeItem>
+            <hr>
+            <ul class="list mx-12">
+                <li v-if="products.length <= 0">
+                    <h3>Sua lista está vazia.</h3>
+                </li>
+                <li class="list__prod list__title" v-else>
+                    <strong class="list__name-prod mx-3">
+                        Produto:
+                    </strong>
 
-                <strong class="list__qtd-prod col mx-2">
-                    Qtd:
-                </strong>
+                    <strong class="list__qtd-prod col mx-2">
+                        Qtd:
+                    </strong>
 
-                <strong class="list__price-uni col mx-3">
-                    Preço Unitário:
-                </strong>
+                    <strong class="list__price-uni col mx-3">
+                        Preço Unitário:
+                    </strong>
 
-                <strong class="list__price-total col mx-3">
-                    Total:
-                </strong>
+                    <strong class="list__price-total col mx-3">
+                        Total:
+                    </strong>
 
-                <div class="list__remve-product col mx-1"></div>
-            </li>
-            <li class="list__prod" v-for="(product, index) in allProducts" v-bind:key="index" v-bind:id="'product-'+index">
-                <input type="checkbox" class="list__checkbox" v-bind:id="index">
-                <label class="list__name-prod list__label-prod mx-3 sm-5 ph-2" v-bind:for="index">
-                    {{ product.nome }}
-                </label>
+                    <div class="list__remve-product col mx-1"></div>
+                </li>
+                <li class="list__prod" v-for="(product, index) in allProducts" v-bind:key="index" v-bind:id="'product-'+index">
+                    <input type="checkbox" class="list__checkbox" v-bind:id="index">
+                    <label class="list__name-prod list__label-prod mx-3 sm-5 ph-2" v-bind:for="index">
+                        {{ product.nome }}
+                    </label>
 
-                <div class="list__qtd-prod col mx-2 sm-1 ph-1">
-                    <input type="number" v-bind:id="'qtd'+index" v-model="product.quantidade" v-on:keyup="incluirPreco(index)" v-on:blur="requireQtd(index)" min="1" required>
-                </div>
+                    <div class="list__qtd-prod col mx-2 sm-1 ph-1">
+                        <input type="number" v-bind:id="'qtd'+index" v-model="product.quantidade" v-on:keyup="incluirPreco(index)" v-on:blur="requireQtd(index)" min="1" required>
+                    </div>
 
-                <div class="list__price-uni col mx-3 sm-2 ph-1">
-                    <input type="number" pattern="[0-9]+([,\.][0-2]+)?" name="price-product" placeholder="Valor (R$):" step="0.01" v-bind:id="'price-'+index" v-on:keyup="incluirPreco(index)" v-model="product.preco">
-                </div>
+                    <div class="list__price-uni col mx-3 sm-2 ph-1">
+                        <input type="number" pattern="[0-9]+([,\.][0-2]+)?" name="price-product" placeholder="Valor (R$):" step="0.01" v-bind:id="'price-'+index" v-on:keyup="incluirPreco(index)" v-model="product.preco">
+                    </div>
 
-                <div class="list__price-total col mx-3 sm-3 ph-1">
-                    {{ product.valortotal }}
-                </div>
+                    <div class="list__price-total col mx-3 sm-3 ph-1">
+                        {{ product.valortotal }}
+                    </div>
 
-                <div class="list__remve-product col mx-1 sm-1 ph-1">
-                    <button type="button" name="button" class="list__btn" v-on:click.prevent="removeProduct(index)" title="Remover">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path>
-                        </svg>
-                    </button>
-                </div>
-            </li>
-        </ul>
+                    <div class="list__remve-product col mx-1 sm-1 ph-1">
+                        <button type="button" name="button" class="list__btn" v-on:click.prevent="removeProduct(index)" title="Remover">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+            </ul>
 
-        <footer class="footer">
-            <p>Total da compra: <strong>R${{ totalvalor }}</strong></p>
-        </footer>
+            <footer class="footer">
+                <p>Total da compra: <strong>R${{ totalvalor }}</strong></p>
+            </footer>
+        </div>
+        <ModalDuplicidade 
+         v-if="duplicidade" 
+         :duplicidade="duplicidade" 
+         v-on:alter-qdt="fecharModalDuplicidade" 
+         v-on:remove-modal="removeProduct" />
     </div>
 </template>
 
 <script>
 import IncludeItem from './IncludeItem.vue'
-
+import ModalDuplicidade from './ModalDuplicidade'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
 	name: 'ListaProdutos',
 	components: {
-		IncludeItem
+		IncludeItem,
+        ModalDuplicidade
 	},
 	data(){
-		return {
-			products: [],
-			totalprecos: [],
-			totalvalor: 0
-		}
-	},
+        return {
+            products: [],
+            totalprecos: [],
+            totalvalor: 0,
+            duplicidade: ''
+        }
+    },
 	created(){
 		if(localStorage.getItem('productsList')!=null) {
             this.products = JSON.parse(localStorage.getItem('productsList'));
@@ -89,6 +98,22 @@ export default defineComponent({
         }
 	},
 	methods: {
+        hasProd(product){
+            const nameNewProd = product.nome.toLowerCase();
+            
+            const hastThisProd = this.products.findIndex((prod)=> prod.nome.toLowerCase() === nameNewProd);
+
+            if(hastThisProd < 0) {
+                this.addProduct(product);
+            } else {
+                this.duplicidade = {
+                    currentArray: hastThisProd,
+                    prodNome: this.products[hastThisProd].nome,
+                    prodQtd: +this.products[hastThisProd].quantidade,
+                }
+                navigator.vibrate(400)
+            }
+        },
     	addProduct(product){
     		this.products.push(product);
     		localStorage.setItem('productsList', JSON.stringify(this.products));
@@ -96,6 +121,7 @@ export default defineComponent({
     	removeProduct(index){
     		this.products.splice(index, 1);
     		this.totalprecos.splice(index, 1);
+            this.duplicidade = ''
 
 			if(this.products.length) {
 				//alert('ainda tem produto(s)');
@@ -135,7 +161,20 @@ export default defineComponent({
 
 			localStorage.setItem('productsList', JSON.stringify(this.products));
 			localStorage.setItem('totalvalor', this.totalvalor);
-		}
+		},
+        fecharModalDuplicidade(change){
+            let newQtd = change.newQtd
+            let currentArray = change.currentArray
+            let qtdAtual = +this.products[currentArray].quantidade
+
+            if(newQtd != qtdAtual) {
+                this.products[currentArray].quantidade = newQtd
+
+                this.requireQtd(currentArray)
+            }
+
+            this.duplicidade = ''
+        }
 	},
 	computed: {
 		allProducts(){
---
+++ src/components/ModalDuplicidade.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="modal -show">
+        <div class="modal__content">
+            <header class="modal__header">
+                <span class="modal__title">PRODUTO DUPLICADO!</span>
+                <!--<button class="modal__close" aria-label="delete"></button>-->
+            </header>
+            <div class="modal__body">
+                <h3>Já tem {{ duplicidade.prodNome }} na sua lista.</h3>
+                <br>
+                <label>Deseja alterar a quantidade?</label>
+                <fieldset class="modal__qtd"> 
+                    <button class="modal__qtd-btn" v-on:click="reduzir" :disabled="newQtd < 2 ? true : false">-</button>
+                    <input class="modal__qtd-ipt" type="number" min="1" v-on:blur="min" v-model="newQtd" required>
+                    <button class="modal__qtd-btn" v-on:click="acrescentar">+</button>
+                </fieldset>
+            </div>
+            <footer class="modal__footer">
+                <button v-on:click="salvar">
+                    {{ newQtd === duplicidade.prodQtd ? 'Fechar' : 'Salvar' }}
+                </button>
+                <button v-on:click="remover" class="modal__delete">
+                    Excluir
+                </button>
+            </footer>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'ModalDuplicidade',
+    props: {
+        duplicidade: {
+            type: Object,
+            default: () => {}
+        },
+    },
+    data(){
+        return {
+            newQtd: this.duplicidade.prodQtd ? this.duplicidade.prodQtd : 1
+        }
+    },
+    methods: {
+        acrescentar(){
+            this.newQtd = this.newQtd + 1
+        },
+        reduzir(){
+            this.newQtd > 1 ? this.newQtd = this.newQtd - 1 : this.newQtd
+        },
+        salvar(){
+            this.$emit('alter-qdt', {
+                newQtd: this.newQtd,
+                prodQtd: this.duplicidade.prodQtd,
+                currentArray: this.duplicidade.currentArray
+            })
+        },
+        remover(){
+            this.$emit('remove-modal', this.duplicidade.currentArray)
+        },
+        min(){
+            this.newQtd < 1 ? this.newQtd = 1 : this.newQtd
+        }
+    }
+})
+</script>
